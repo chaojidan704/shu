e9f899457cb58de3f9aae67d754d7b42cec07ea9
xyo-ovo <2648247511@qq.com>
2026-04-07 12:39:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,28 @@
+const CACHE_NAME = "shu-pwa-v2";
+const ASSETS = [
+  "/shu/",
+  "/shu/index.html",
+  "/shu/manifest.webmanifest",
+  "/shu/icon-192x192.png",
+  "/shu/icon-512x512.png"
+];
+
 self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
